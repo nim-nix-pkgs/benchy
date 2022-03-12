049439bfd1eb556e94ffedf4959d165696186d9f
hugosenari <hugosenari@gmail.com>
2022-03-12 00:45:21 -0300

    chore: re index benchy

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Simple benchmarking to time your code.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."benchy-master".url = "path:./master";
+  inputs."benchy-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."benchy-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
